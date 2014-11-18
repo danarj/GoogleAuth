c771b70d58fdcbd45ddb98030c59e93c03150f1c
danarj <danarj@live.com>
2014-11-18 18:26:29 +0300

    start

---
+++ MvcAuthGoogle/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using Microsoft.Web.WebPages.OAuth;
+
+namespace MvcAuthGoogle.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Error()
+        {
+            return View();
+        }
+        public void ExternalLogin(string provider)
+        {
+            // just pass a provider to make it work
+            OAuthWebSecurity.RequestAuthentication(provider, Url.Action("ExternalLoginCallback"));
+        }
+        public ActionResult ExternalLoginCallback()
+        {
+            var result = OAuthWebSecurity.VerifyAuthentication();
+
+            if (result.IsSuccessful == false)
+            {
+                return RedirectToAction("Error", "Account");
+            }
+
+            // this is just demo. Make all things you need before setting cookies
+            FormsAuthentication.SetAuthCookie(result.UserName, false);
+
+            // We need to get the url user has entered to the browser to use it in the redirect. 
+            // This is just demo.
+            return Redirect(Url.Action("Index", "Secure"));
+        }
+    }
+}
---
+++ MvcAuthGoogle/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcAuthGoogle.MvcApplication" Language="C#" %>
